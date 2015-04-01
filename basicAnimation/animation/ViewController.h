d0061983ee95f7317f45d79e29f37211b7fa2afe
truthbeauty <303040805@qq.com>
2015-04-01 14:52:01 +0800

    一个简单的运动动画。

---
+++ basicAnimation/animation/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  animation
+//
+//  Created by sbx_fc on 15-3-30.
+//  Copyright (c) 2015年 RG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property(nonatomic, weak) IBOutlet UILabel *label1;
+@property(nonatomic, weak) IBOutlet UILabel *label2;
+@property(nonatomic, weak) IBOutlet UILabel *label3;
+
+
+@end
+
---
+++ basicAnimation/animation/ViewController.m
@@ -0,0 +1,92 @@
+//
+//  ViewController.m
+//  animation
+//
+//  Created by sbx_fc on 15-3-30.
+//  Copyright (c) 2015年 RG. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+{
+    UIImageView* tank;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+     /**
+     * 使用定时器实时更新信息
+     */
+    NSTimer* myTimer = [NSTimer scheduledTimerWithTimeInterval:0.1f target:self selector:@selector(update:) userInfo:nil repeats:YES];
+    [[NSRunLoop mainRunLoop] addTimer:myTimer forMode:NSRunLoopCommonModes];
+    
+    /**
+     * 坦克
+     */
+    UIImage* image = [UIImage imageNamed:@"tank"];
+    tank = [[UIImageView alloc]initWithImage:image];
+    tank.backgroundColor = [UIColor blackColor];
+    tank.center = CGPointMake(10 + image.size.width/2, self.label3.frame.origin.y + self.label3.frame.size.height + 10 + image.size.height/2);
+    tank.image = image;
+    [self.view addSubview:tank];
+    
+     /**
+     * 简单的移动动画
+     */
+    CABasicAnimation *animation = [CABasicAnimation animation];
+    animation.keyPath = @"position.x";
+    animation.fromValue = @10;
+    animation.toValue = @455;
+    animation.duration = 10.f;
+     /**
+     * 设置以下两个属性,可以让图片停在动画末端
+     * 但是,让一个动画维持一个状态会很消耗性能。
+     */
+//    animation.fillMode = kCAFillModeForwards;
+//    animation.removedOnCompletion = NO;
+    [tank.layer addAnimation:animation forKey:@"basic"];
+}
+
+ /**
+ * 刷新图片位置信息
+ */
+-(void)update:(NSTimer*)timer
+{
+     /**
+     * 显示 presentation layer 的位置
+     */
+    CALayer *layer = [tank.layer presentationLayer];
+    NSString* text = [NSString stringWithFormat:@"presentation layer [%f,%f,%f,%f]",layer.frame.origin.x,layer.frame.origin.y,layer.frame.size.width,layer.frame.size.height];
+    self.label1.text = text;
+    [self.label1 setNeedsDisplay];
+    
+     /**
+     * 显示 model layer 的位置
+     */
+    layer = [tank.layer modelLayer];
+    text = [NSString stringWithFormat:@"model layer [%f,%f,%f,%f]",layer.frame.origin.x,layer.frame.origin.y,layer.frame.size.width,layer.frame.size.height];
+    self.label2.text = text;
+    [self.label2 setNeedsDisplay];
+    
+     /**
+     * 显示 view 的位置
+     */
+    layer = tank.layer;
+    text = [NSString stringWithFormat:@"frame [%f,%f,%f,%f]",layer.frame.origin.x,layer.frame.origin.y,layer.frame.size.width,layer.frame.size.height];
+    self.label3.text = text;
+    [self.label3 setNeedsDisplay];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
